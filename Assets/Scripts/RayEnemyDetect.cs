2ddca985c17745057c4d5413087792561adff906
Owen <owengoldthwaite@gmail.com>
2019-09-29 00:56:02 +0200

    Raycast enemy detection working
    Title + some stuff to try and render view distances, not complete yet

--- Assets/Scripts/RayEnemyDetect.cs
+++ Assets/Scripts/RayEnemyDetect.cs
@@ -1,55 +1,124 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class RayEnemyDetect : MonoBehaviour
 {
-    public float viewangle;
-    public int raycount;
+    public float viewAngle;
+    public int rayCount;
+    public float lookDistance;
 
 
+    private LineRenderer viewLineRenderer;
     private List<RaycastHit2D> rays;
+    private float minViewAngle;
+    private float maxViewAngle;
+    private int layerMask;
     // Start is called before the first frame update
     void Start()
     {
-        rays = new List<RaycastHit2D>(raycount);
+        rays = new List<RaycastHit2D>(rayCount);
+
+        viewLineRenderer = GetComponent<LineRenderer>();
+        viewLineRenderer.positionCount = rayCount;
+
+        maxViewAngle = viewAngle;
+        minViewAngle = -viewAngle;
+        rayCount /= 2;
+
+
+        layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        CreateRays();
+        CheckRays(CreateRays());
     }
 
-    private void CreateRays()
+    private void CheckRays(List<Vector3> renderVectors)
     {
 
-        for (int i = 0; i < raycount; i++)
+        int count = 0;
+
+        foreach (RaycastHit2D hit in rays)
         {
-            print("ran");
-            var increment = viewangle / raycount;
-            var fowardangle = transform.eulerAngles.z;
+            if(hit.collider != null)
+            {
+                if (hit.collider.tag == "Player")
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
-            Vector2 dir1 = DegreeToVector2(fowardangle + ((viewangle - increment) * (i / 2)));
-            Vector2 dir2 = DegreeToVector2(fowardangle - ((viewangle + increment) * (i / 2)));
+                renderVectors[count++] = hit.point;
 
-            if (Vector2.Angle(transform.right, dir1) > viewangle)
-                continue;
+                //for(int i = 0; i < 5; i++)
+                //{
+                //    if (!(count + i > renderVectors.Count - 1))
+                //        renderVectors[count + i] = hit.point;
 
+                //    if (!(count - 1 < 0))
+                //        renderVectors[count - i] = hit.point;
 
-            Debug.DrawRay(transform.position, dir1);
-            Debug.DrawRay(transform.position, dir2);
+                //    count++;
+                //}
+                
+
+            }
         }
+
+        viewLineRenderer.SetPositions(renderVectors.ToArray());
+        rays.Clear();
     }
 
-
-    private Vector2 RadianToVector2(float radian)
+    private List<Vector3> CreateRays()
     {
-        return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
+
+        float increment = viewAngle / rayCount;
+        float angle1 = 0;
+        float angle2 = 0;
+
+        List<Vector3> renderVectors = new List<Vector3>();
+
+        for (int i = 0; i < rayCount; i++)
+        {
+
+            angle1 += increment;
+            angle2 -= increment;
+
+            if (angle1 > maxViewAngle)
+                angle1 = maxViewAngle;
+            if (angle2 < minViewAngle)
+                angle2 = minViewAngle;
+
+            Vector3 dir1 = Quaternion.AngleAxis(angle1, transform.forward) * transform.right;
+            Vector3 dir2 = Quaternion.AngleAxis(angle2, transform.forward) * transform.right;
+
+            rays.Add(Physics2D.Raycast(transform.position, dir1, lookDistance, layerMask));
+            rays.Add(Physics2D.Raycast(transform.position, dir2, lookDistance, layerMask));
+
+            renderVectors.Add(transform.position + (dir1 * lookDistance));
+            renderVectors.Add(transform.position + (dir2 * lookDistance));
+
+
+            Debug.DrawRay(transform.position, dir1, Color.green);
+            Debug.DrawRay(transform.position, dir2, Color.red);
+        }
+
+        return renderVectors;
+
+        //print(renderVectors[0]);
+        //viewLineRenderer.SetPosition(0, transform.position);
+        //viewLineRenderer.SetPosition(1, renderVectors[0]);
+        //viewLineRenderer.SetPosition(2, transform.position);
+        //viewLineRenderer.SetPosition(3, renderVectors[1]);
+        //viewLineRenderer.SetPosition(4, transform.position);
+
+
+        //viewLineRenderer.SetPositions(renderVectors);
+
     }
 
-    private Vector2 DegreeToVector2(float degree)
-    {
-        return RadianToVector2(degree * Mathf.Deg2Rad);
-    }
 }
