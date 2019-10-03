f06a3ef8252c94d9ab1c87076bdd0eea01ebe2af
Matthew <mritter4@villanova.edu>
2019-10-03 15:29:26 +0200

    Some Sstuff
    stuff

---
+++ Assets/Scripts/AlertNear.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertNear : MonoBehaviour
+{
+
+    public float AlertRadius;
+
+    private CircleCollider2D alertArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alertArea = gameObject.AddComponent<CircleCollider2D>();
+        alertArea.radius = AlertRadius;
+        alertArea.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        print("Running stay");
+        if (collision.gameObject.tag == "Enemy")
+        {
+            if (collision.gameObject.GetComponent<WaypointPatrol>() != null)
+            {
+                collision.gameObject.GetComponent<WaypointPatrol>().SetAlertState(true);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Running enter");
+        if (collision.gameObject.tag == "Enemy")
+        {
+            if (collision.gameObject.GetComponent<WaypointPatrol>() != null)
+            {
+                collision.gameObject.GetComponent<WaypointPatrol>().SetAlertState(true);
+            }
+        }
+    }
+
+   
+
+
+}
